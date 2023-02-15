8cc9386a104f6c492d0338a79161b81e99f6ec6b
Ebutkut <erika.butkut@gmail.com>
2023-02-15 17:11:44 +0200

    Add changes to solution task 9 subtask 16

--- Uzduotis16/Program.cs
+++ Uzduotis16/Program.cs
@@ -20,6 +20,8 @@ namespace Uzduotis16
 
             //Raskite visų lyginių skaičių nuo 20 iki 40 sumą.
 
+            suma = 0;
+
             for (int i = 20; i < 41; i++)
             {
                 if (i % 2 == 0)
@@ -32,9 +34,11 @@ namespace Uzduotis16
 
             // Raskite visų nelyginių skaičių nuo 30 iki 60 sumą.
 
+            suma = 0;
+
             for (int i = 30; i < 61; i++)
             {
-                if (i % 3 == 0)
+                if (i% 2== 1)
                 {
                     suma += i;
                 }
@@ -49,6 +53,8 @@ namespace Uzduotis16
              * Šių skaičių suma yra 23.
              * Turite gauti 233168 atsakymą.
              */
+
+            suma = 0;
 
             for (int i = 0; i < 1000; i++)
             {
