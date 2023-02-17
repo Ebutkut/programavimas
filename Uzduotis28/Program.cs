9300efcd66f6da14849c2f65e069077ccde10d0d
Ebutkut <erika.butkut@gmail.com>
2023-02-17 18:16:09 +0200

    Add solution to task 10 subtask 28

---
+++ Uzduotis28/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Uzduotis28
+
+    // Aprašykite ir iškvieskite metodą, kuris išvestų bet kokį jūsų pasirinktą tekstą.
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Start();
+        }
+
+        private static void Start()
+        {
+            Console.WriteLine("PROGRAMA PRADĖJO DARBĄ");
+        }
+    }
+}
