b78a39ac573ea48fc81c7dc9d67aa7a993b1eb5e
Ebutkut <erika.butkut@gmail.com>
2023-02-17 17:51:53 +0200

    Add solution to task 10 subtask 29

---
+++ Uzduotis29/Greetings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzduotis29
+{
+    internal class Greetings
+    {
+        
+        internal static void GreetUserByNameAndAge(string name, string age)
+        {
+            Console.WriteLine($"Labas, {name} ({age} m.).");
+            
+        }
+
+        internal static void GreetByHobby(string hobby)
+        {
+            Console.WriteLine($"Jūsų įrašytas hobis: {hobby}");
+        }
+
+    }
+}
---
+++ Uzduotis29/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Uzduotis29
+
+    /*
+     * Parašykite metodą, kuris pasisveikintų su vartotoju.
+     * Vartotojas turi įvesti savo vardą, amžių ir hobio pavadinimą.
+     * Visi šie duomenys yra duodami metodui, kuris visą šią informaciją išveda formatuotai
+     */
+
+{
+    internal class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            
+            Console.WriteLine("Jūsų vardas ");
+            string name = Console.ReadLine();
+           
+            Console.WriteLine("Jūsų amžius ");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Hobio pavadinimas ");
+            string hobby = Console.ReadLine();
+
+            Greetings.GreetUserByNameAndAge(name, age);
+            Greetings.GreetByHobby(hobby);
+        }
+    }
+}
